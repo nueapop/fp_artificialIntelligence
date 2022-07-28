45353b400bff6223acf47891f8daea7b1104992a
nueapop <nueapop.mo@gmail.com>
2022-07-29 04:05:14 +0700

    update: main.py & gitignore

---
+++ pytorch_cnn/main.py
@@ -0,0 +1,15 @@
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+train_data = datasets.MNIST(
+    root = 'data',
+    train = True,                         
+    transform = ToTensor(), 
+    download = True,            
+)
+test_data = datasets.MNIST(
+    root = 'data', 
+    train = False, 
+    transform = ToTensor()
+)
+print(train_data)
+print(test_data)

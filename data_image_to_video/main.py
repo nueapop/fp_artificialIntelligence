0289577e601deb74a0fc9aec2abafd63940dc15d
nueapop <nueapop.mo@gmail.com>
2022-07-29 01:02:42 +0700

    update: .gitignore & main.py

--- data_image_to_video/main.py
+++ data_image_to_video/main.py
@@ -1,18 +1,45 @@
-import cv2
 import os
+import cv2
+from rich.console import Console
+from rich.prompt import Prompt
 
-image_folder = 'D:/Works/STUDY/PJ_IOT/fp_artificialIntelligence/serial_mlx90640_scraping/data/MLX90640_TWO-PERSON_WALLVIEW/images'
-video_name = 'video.mp4'
+console = Console()
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
+class searchPath:
+    def __init__(self):
+        pass
 
-video = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), 6, (width,height))
+    def process(self):
+        console.print("Folder Scraping Lists:", style="bold Yellow")
+        for i in os.listdir("../"):
+            if "scraping" in i:
+                console.print(i, style="green")
+        mainPath = "../" + Prompt.ask("[bold cyan]Select path[/bold cyan] ") + "/data/"
+        for i in os.listdir(mainPath):
+            console.print(i, style="green")
+        childPath = Prompt.ask("[bold cyan]Select path[/bold cyan] ") + "/images"
+        return mainPath + childPath
 
-for image in images:
-    print(image)
-    video.write(cv2.imread(os.path.join(image_folder, image)))
+class imageToVideo:
+    def __init__(self):
+        self.path = searchPath().process()
+        self.videoName = "data/" + self.path.replace("../", "").replace("/data/", "_").replace("/images", "") + ".mp4"
+        self.framerate = int(Prompt.ask("[bold cyan]Framerate[/bold cyan] "))
+        self.images = [img for img in os.listdir(self.path) if img.endswith(".png")]
+        self.frame = cv2.imread(os.path.join(self.path, self.images[0]))
+        self.height, self.width, self.channels = self.frame.shape
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        self.out = cv2.VideoWriter(self.videoName, self.fourcc, self.framerate, (self.width, self.height))
 
-cv2.destroyAllWindows()
-video.release()
+    def process(self):
+        for i in self.images:
+            self.out.write(cv2.imread(os.path.join(self.path, i)))
+            console.print("Processing: " + i, style="green")
+        self.out.release()
+
+if __name__ == "__main__":
+    try:
+        imageToVideo().process()
+    except:
+        console.print("Error", style="bold red")
+        imageToVideo().process()
